3deeaee5a0f36e399385165a66f644e34f1754e7
AzuxDario <pawel_mibor95@tlen.pl>
2018-10-05 00:31:07 +0200

    Initial commit

---
+++ MiraiZuraBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiraiZuraBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
