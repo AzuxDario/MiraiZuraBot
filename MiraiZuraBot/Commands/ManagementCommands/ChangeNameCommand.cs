f11fe73cef2d5857828e5fe79d878995716b20f3
AzuxDario <pawel_mibor95@tlen.pl>
2018-10-05 01:34:21 +0200

    Add 2 more commands

---
+++ MiraiZuraBot/Commands/ManagementCommands/ChangeNameCommand.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using MiraiZuraBot.Attributes;
+using MiraiZuraBot.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiraiZuraBot.Commands.ManagementCommands
+{
+    [CommandsGroup("Management")]
+    class ChangeNameCommand : BaseCommandModule
+    {
+        [Command("changeName")]
+        [Description("Change bot descrition.")]
+        public async Task ChangeName(CommandContext ctx, [Description("New name.")] string name = null)
+        {
+            if (ctx.Member.Id == Bot.configJson.Developer)
+            {
+                await Bot.DiscordClient.UpdateCurrentUserAsync(name);
+            }
+        }
+    }
+}
---
+++ MiraiZuraBot/Commands/ManagementCommands/DescriptionCommand.cs
@@ -0,0 +1,52 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using MiraiZuraBot.Attributes;
+using MiraiZuraBot.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MiraiZuraBot.Commands.ManagementCommands
+{
+    [CommandsGroup("Management")]
+    class DescriptionCommand : BaseCommandModule
+    {
+        private Timer refreshDescriptionTimer;
+        private int refreshDescriptionInterval;
+
+        private string game;
+
+        public DescriptionCommand()
+        {
+            refreshDescriptionInterval = 1000 * 60;    // every 1 minute
+            refreshDescriptionTimer = new Timer(RefreshDescriptionCallback, null, refreshDescriptionInterval, Timeout.Infinite);
+
+            game = string.Empty;
+        }
+
+        [Command("description")]
+        [Description("Change bot descrition.")]
+        public async Task Description(CommandContext ctx, [Description("New description.")] string description = null)
+        {
+            if (ctx.Member.Id == Bot.configJson.Developer)
+            {
+                game = description;
+
+                await Bot.DiscordClient.UpdateStatusAsync(new DiscordActivity(description));
+            }
+        }
+
+        private void RefreshDescriptionCallback(object state)
+        {
+            if (game != string.Empty)
+            {
+                Bot.DiscordClient.UpdateStatusAsync(new DiscordActivity(game));  
+            }
+
+            refreshDescriptionTimer.Change(refreshDescriptionInterval, Timeout.Infinite);
+        }
+    }
+}
