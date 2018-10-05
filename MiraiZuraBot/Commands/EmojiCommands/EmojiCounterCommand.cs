76b015ba7a01e966762ebfa68b44e5feaebd9b38
AzuxDario <pawel_mibor95@tlen.pl>
2018-10-06 00:46:37 +0200

    Shows all emoji in server during counting

--- MiraiZuraBot/Commands/EmojiCommands/EmojiCounterCommand.cs
+++ MiraiZuraBot/Commands/EmojiCommands/EmojiCounterCommand.cs
@@ -25,35 +25,74 @@ namespace MiraiZuraBot.Commands.EmojiCommands
                 Server dbServer = databaseContext.Servers.Where(p => p.ServerID == ctx.Guild.Id.ToString()).Include(p => p.Emojis).FirstOrDefault();
                 if (dbServer != null)
                 {
-                    if (dbServer.Emojis != null)
+
+                    IReadOnlyList<DiscordGuildEmoji> serverEmojiList;
+                    // List for emojis to print
+                    List<EmojiHolder> emojiHolderList = new List<EmojiHolder>();
+                    // Get server emoji
+                    serverEmojiList = await ctx.Guild.GetEmojisAsync();
+
+                    // Get emoji from database
+                    List<Emoji> dbEmojis = dbServer.Emojis.OrderBy(p => p.UsageCount).ToList();
+
+                    foreach (DiscordGuildEmoji emoji in serverEmojiList)
                     {
-                        List<Emoji> emojis = dbServer.Emojis.OrderBy(p => p.UsageCount).ToList();
-                        string wholeMessage = "";
-
-                        foreach (Emoji emoji in emojis)
+                        if(emoji.IsAnimated == true)
                         {
-                            try
-                            {
-                                DiscordGuildEmoji discordEmoji = await ctx.Guild.GetEmojiAsync(ulong.Parse(emoji.EmojiID));
-                                string message = "<:" + discordEmoji.Name + ":" + discordEmoji.Id + ">" + " użyto: " + emoji.UsageCount + " razy \n";
-                                wholeMessage += message;
-                            }
-                            catch
-                            {
-                                //dupa
-                            }
-                        }
-                        if (wholeMessage != null)
-                        {
-                            await ctx.RespondAsync(wholeMessage);
+                            continue;
                         }
 
+                        Emoji tempEmoji = dbEmojis.Where(p => p.EmojiID == emoji.Id.ToString()).FirstOrDefault();
+                            
+                        if(tempEmoji != null)
+                        {
+                            EmojiHolder emojiHolder = new EmojiHolder(emoji.Name, emoji.Id, tempEmoji.UsageCount);
+                            emojiHolderList.Add(emojiHolder);
+                        }
+                        else
+                        {
+                            EmojiHolder emojiHolder = new EmojiHolder(emoji.Name, emoji.Id, 0);
+                            emojiHolderList.Add(emojiHolder);
+                        }
+                    }
+
+                    string wholeMessage = "";
+                    emojiHolderList = emojiHolderList.OrderByDescending(p => p.UsageCount).ToList();
+
+                    foreach (EmojiHolder emoji in emojiHolderList)
+                    {
+                        wholeMessage += emoji.GetEmojiToSend() + "\n";
+                    }
+
+                    if (wholeMessage != null)
+                    {
+                        await ctx.RespondAsync(wholeMessage);
                         return;
                     }
+
                 }
 
                 await ctx.RespondAsync("Na tym serwerze jeszcze nie użyto emoji.");
             }
+        }
+    }
+
+    public class EmojiHolder
+    {
+        public string EmojiName { get; set; }
+        public ulong EmojiID { get; set; }
+        public int UsageCount { get; set; }
+
+        public EmojiHolder(string name, ulong id, int count)
+        {
+            EmojiName = name;
+            EmojiID = id;
+            UsageCount = count;
+        }
+
+        public string GetEmojiToSend()
+        {
+            return "<:" + EmojiName + ":" + EmojiID + "> użyto: " + UsageCount;
         }
     }
 }
