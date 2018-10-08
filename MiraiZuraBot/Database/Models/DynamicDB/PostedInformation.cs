933ba9d6b7153fda0798ee6228093df8343e4c74
AzuxDario <pawel_mibor95@tlen.pl>
2018-10-08 23:50:40 +0200

    Add year do posted informations

--- MiraiZuraBot/Database/Models/DynamicDB/PostedInformation.cs
+++ MiraiZuraBot/Database/Models/DynamicDB/PostedInformation.cs
@@ -9,6 +9,7 @@ namespace MiraiZuraBot.Database.Models.DynamicDB
         public int ID { get; set; }
         public int Day { get; set; }
         public int Month { get; set; }
+        public int Year { get; set; }
 
         public int ChannelID { get; set; }
         public virtual Channel Channel { get; set; }
