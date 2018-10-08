06c35f512d41b9b818b550a51c2bf2165ba46ed7
AzuxDario <pawel_mibor95@tlen.pl>
2018-10-08 23:22:52 +0200

    Add announement class

---
+++ MiraiZuraBot/Commands/AnnouncementCommands/AnnouncementCommand.cs
@@ -0,0 +1,27 @@
+﻿using DSharpPlus.CommandsNext;
+using MiraiZuraBot.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MiraiZuraBot.Commands.AnnouncementCommands
+{
+    [CommandsGroup("Powiadamianie")]
+    class AnnouncementCommand : BaseCommandModule
+    {
+        private Timer checkMessagesTimer;
+        private int checkMessagesInterval;
+
+        public AnnouncementCommand()
+        {
+            checkMessagesInterval = 1000 * 60 * 60;    // every hour
+            checkMessagesTimer = new Timer(RefreshCurrentSongMessages, null, checkMessagesInterval, Timeout.Infinite);
+        }
+
+        private async void RefreshCurrentSongMessages(object state)
+        {
+            return;
+        }
+    }
+}
