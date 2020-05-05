ed1d1bc616fcfa8e83721c363dfe56eac0d0c9a0
AzuxDario <pawel_mibor95@tlen.pl>
2020-05-05 12:14:53 +0200

    Added bot name in console

--- MiraiZuraBot/Core/Bot.cs
+++ MiraiZuraBot/Core/Bot.cs
@@ -19,6 +19,12 @@ namespace MiraiZuraBot.Core
 {
     class Bot
     {
+#if DEBUG
+        readonly string botname = "Mirai Zura Test";
+#else
+        readonly string botname = "Mirai Zura";
+#endif
+
         public struct ConfigJson
         {
             [JsonProperty("token")]
@@ -185,14 +191,14 @@ namespace MiraiZuraBot.Core
 
         private Task Commands_CommandExecuted(CommandExecutionEventArgs e)
         {
-            e.Context.Client.DebugLogger.LogMessage(LogLevel.Info, "ExampleBot", $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'", DateTime.Now);
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Info, botname, $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'", DateTime.Now);
 
             return Task.FromResult(0);
         }
 
         private async Task Commands_CommandErrored(CommandErrorEventArgs e)
         {
-            e.Context.Client.DebugLogger.LogMessage(LogLevel.Error, "ExampleBot", $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
+            e.Context.Client.DebugLogger.LogMessage(LogLevel.Error, botname, $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it errored: {e.Exception.GetType()}: {e.Exception.Message ?? "<no message>"}", DateTime.Now);
 
             switch (e.Exception)
             {
