190437ac3d563f6b7f800795e863502dff669e3a
AzuxDario <pawel_mibor95@tlen.pl>
2020-09-11 18:46:02 +0200

    Fixed images not appear

--- MiraiZuraBot/Helpers/PostEmbedHelper.cs
+++ MiraiZuraBot/Helpers/PostEmbedHelper.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace MiraiZuraBot.Helpers
 {
@@ -11,14 +12,26 @@ namespace MiraiZuraBot.Helpers
     {
         public static async Task PostEmbed(CommandContext ctx, string title = null, string description = null, string imageLink = null, string thumbnailLink = null, DiscordEmbedBuilder.EmbedFooter footer = null)
         {
+            // Discord can't handle links with japanese characters
             if (imageLink != null)
             {
                 imageLink = imageLink.Replace(" ", "%20");
+                int pos = imageLink.LastIndexOf("/");
+                string toChange = imageLink.Substring(pos);
+                string changed = HttpUtility.UrlEncode(toChange, Encoding.UTF8);
+                imageLink = imageLink.Remove(pos);
+                imageLink += changed;
             }
             if(thumbnailLink != null)
             {
                 thumbnailLink = thumbnailLink.Replace(" ", "%20");
+                int posThumbnail = thumbnailLink.LastIndexOf("/");
+                string toChangeThumbnail = imageLink.Substring(posThumbnail);
+                string changedThumbnail = HttpUtility.UrlEncode(toChangeThumbnail, Encoding.UTF8);
+                thumbnailLink = thumbnailLink.Remove(posThumbnail);
+                thumbnailLink += changedThumbnail;
             }
+
             var embed = new DiscordEmbedBuilder
             {
                 Color = new DiscordColor("#00a8ff"),
@@ -30,6 +43,5 @@ namespace MiraiZuraBot.Helpers
             };
             await ctx.RespondAsync(null, false, embed);
         }
-
     }
 }   
