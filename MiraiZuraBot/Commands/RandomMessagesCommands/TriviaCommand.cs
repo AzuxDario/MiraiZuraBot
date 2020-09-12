017c9b8c2984a108b35eff2e84fec584563e4422
AzuxDario <pawel_mibor95@tlen.pl>
2020-09-12 20:36:12 +0200

    Added trivieTopic Command

---
+++ MiraiZuraBot/Commands/RandomMessagesCommands/TriviaCommand.cs
@@ -0,0 +1,32 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using MiraiZuraBot.Attributes;
+using MiraiZuraBot.Helpers;
+using MiraiZuraBot.Services.TriviaService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiraiZuraBot.Commands.RandomMessagesCommands
+{
+    [CommandsGroup("Tekst")]
+    class TriviaCommand : BaseCommandModule
+    {
+        private TriviaService _triviaService;
+
+        public TriviaCommand(TriviaService triviaService)
+        {
+            _triviaService = triviaService;
+        }
+
+        [Command("tematyCiekawostek")]
+        [Description("Pokazuje tematy ciekawostek.")]
+        public async Task TriviaTopics(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            List<string> topics = _triviaService.GetTopics();
+            await PostLongMessageHelper.PostLongMessage(ctx, topics, "Tematy ciekawostek");
+        }
+    }
+}
