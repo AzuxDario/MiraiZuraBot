e2c0312a3e6b2670bc719266df482df7a26862f7
AzuxDario <pawel_mibor95@tlen.pl>
2020-09-22 18:19:22 +0200

    Added new attributes

---
+++ MiraiZuraBot/Attributes/CommandLangAttribute.cs
@@ -0,0 +1,33 @@
+﻿using MiraiZuraBot.Translators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiraiZuraBot.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    class CommandLangAttribute : Attribute
+    {
+        public string PolishCommand { get; }
+        public string EnglishCommand { get; }
+
+        public CommandLangAttribute(string polish, string english)
+        {
+            PolishCommand = polish;
+            EnglishCommand = english;
+        }
+
+        public string GetCommand(Translator.Language lang)
+        {
+            switch (lang)
+            {
+                case Translator.Language.Polish:
+                    return PolishCommand;
+                case Translator.Language.English:
+                    return EnglishCommand;
+                default:
+                    return EnglishCommand;
+            }
+        }
+    }
+}
---
+++ MiraiZuraBot/Attributes/DescriptionLangAttribute.cs
@@ -0,0 +1,33 @@
+﻿using MiraiZuraBot.Translators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiraiZuraBot.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Parameter)]
+    class DescriptionLangAttribute : Attribute
+    {
+        public string PolishDescription { get; }
+        public string EnglishDescription { get; }
+
+        public DescriptionLangAttribute(string polish, string english)
+        {
+            PolishDescription = polish;
+            EnglishDescription = english;
+        }
+
+        public string GetDescription(Translator.Language lang)
+        {
+            switch (lang)
+            {
+                case Translator.Language.Polish:
+                    return PolishDescription;
+                case Translator.Language.English:
+                    return EnglishDescription;
+                default:
+                    return EnglishDescription;
+            }
+        }
+    }
+}
